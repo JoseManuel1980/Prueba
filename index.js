b16e0493b61dfe09efb25dd51063965421a1ddcb
Josezx <54989153+JoseManuel1980@users.noreply.github.com>
2020-04-02 16:17:59 +0200

    changes
    Signed-off-by: Josezx <54989153+JoseManuel1980@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require('cors');
 
-
-//crear el servidor
+// crear el servidor
 const app = express();
 
-//Conectar a ala BD
+// Conectar a la base de datos
 conectarDB();
 
-//Habilitar cors
+// habilitar cors
 app.use(cors());
 
-// habilitar express.json
+// Habilitar express.json
 app.use( express.json({ extended: true }));
 
-//puerto de la app
-const port = process.env.port || 4000;
+// puerto de la app
+const port = process.env.PORT || 4000;
 
-//Importar rutas
+// Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-
-//arrancar la app
+// arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`);
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
